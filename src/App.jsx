fd749db7877558e2b59ed5d22b84cf6fbb766b56
anjali-patel1 <YOUR_ID+anjali-patel1@users.noreply.github.com>
2026-05-25 14:02:49 +0530

    Initial commit: Chat Bot

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import ChatbotIcon from "./components/chatbotIcon";
+import ChatForm from "./components/ChatForm";
+import ChatMessage from "./components/ChatMessage";
+
+
+const App = () => {
+  const [chatHistory, setChatHistory] = useState([]);
+
+  const generateBotResponse = async (history) => {
+
+    //helper function to update chat history
+   const updateHistory  = (text) => {
+    setChatHistory(prev => [...prev.filter(msg => msg.text !=="Thinking..."), {role: "model", text}]);
+   }
+
+    //formate chat history for API request
+   history = history.map(({role, text}) => ({role, parts: [{text}]}))
+
+     const requestOptions ={
+      method: "POST",
+      headers:{ "Content-Type": "application/json" },
+      body: JSON.stringify({contents: history })
+     };
+
+     try {
+      //Make the API call to get the Bot'sresponse
+         const response = await fetch(import.meta.env.VITE_API_URL, requestOptions);
+         const data = await response.json();
+         if(!response.ok) throw new Error(data.error.message || "Somthing Went Wrong!");
+
+        // clean and update chat history with bot's response
+         const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+         updateHistory(apiResponseText);
+     } catch (error) {
+       console.log(error);
+     }
+  };
+
+  return (
+    <div className="container">
+      <div className="chatbot-popup">
+        {/*chatbot header */}
+        <div className="chat-header">
+          <div className="header-info">
+            <ChatbotIcon />
+            <h2 className="logo-text">ChatBot</h2>
+          </div>
+          <button className="material-symbols-rounded">keyboard_arrow_down</button>
+        </div>
+           
+           {/*chatbot body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <ChatbotIcon />
+            <p className="message-text">
+              Hey there!👋 <br/> how can I help you today?
+            </p>
+          </div>
+
+          {/*render the chathistory dynamically */}
+          {chatHistory.map((chat, index) => (
+            <ChatMessage key={index} chat={chat} />
+          ))}
+        </div>
+
+           {/*chatbot footer */}
+        <div className="chat-footer">
+               <ChatForm chatHistory={chatHistory} setChatHistory={setChatHistory} generateBotResponse={generateBotResponse} />
+        </div>
+      </div>
+    </div>
+  );;
+};
+
+export default App
---
+++ src/components/ChatForm.jsx
@@ -0,0 +1,38 @@
+import { useRef } from "react";
+
+
+const ChatForm = ({chatHistory,  setChatHistory, generateBotResponse}) => {
+    const inputRef = useRef();
+
+
+    const handleFormSubmit = (e) => {
+      e.preventDefault();
+      const userMessage = inputRef.current.value.trim();
+      if (!userMessage) return;
+      inputRef.current.value ="";  
+
+      //update chat history with the user's message
+      setChatHistory((history) => [... history, { role: "user", text: userMessage }]);
+
+      setTimeout(() => {
+                  //Add a thinking placeholder for the bot's response
+            setChatHistory((history) => [... history, { role: "model", text: "Thinking..." }]);
+
+             generateBotResponse([...chatHistory, { role: "user", text: userMessage }]);
+
+      }, 600 ); 
+
+};
+
+return (
+
+<form action="#" className="chat-form" onSubmit={handleFormSubmit}>
+      <input ref={inputRef} type="text" placeholder="Message..." className="message-input" required />
+      <button className="material-symbols-rounded">arrow_upward</button>
+</form>
+
+);
+
+};
+
+export default ChatForm;
